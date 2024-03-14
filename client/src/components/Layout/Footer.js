f91ca904ba76e50523715e1fa4653ac9100aed05
Ayush12521198 <86072815+Ayush12521198@users.noreply.github.com>
2024-03-14 13:39:40 +0530

    Add files via upload

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div style={{ position: "fixed", bottom: 0, width: "100%", backgroundColor: "#343a40", color: "white", padding: "10px" }}>
+      <h6 style={{ textAlign: "center" }}>
+        All rights reserved &copy; Ayush
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
